b23b7fa4eb56e6c9825b65541903f9c5ac05e4a7
1rascu <mantencion_rc_@hotmail.co>
2024-06-09 15:47:32 -0400

    se crea hoja css

---
+++ assets/js/index.js
@@ -0,0 +1,14 @@
+let numerox = 1
+
+console.log("codigo dentro del bloque")
+
+if (numerox < 1){
+    console.log("codigo dentro del bloque")
+}
+
+let temperatura
+
+if (temperatura > 30)
+    {
+        console.log("ya esta haciendo calorrshhh")
+    }
